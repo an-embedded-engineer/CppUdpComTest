ecaef516d7ae7225325f006fa050eeaca8bf4b14
Shin <shin@iMac.local>
2020-08-16 15:05:32 +0900

    Fix Configuration

--- Sender/Sender.cpp
+++ Sender/Sender.cpp
@@ -25,12 +25,12 @@ int main()
 
 #if TEST_MODE == TEST_MODE_UNICAST
         /* UDPユニキャスト送信用ソケットオープン */
-        adapter.OpenUdpUniTxSocket("127.0.0.1", 50000);
+        adapter.OpenUdpUniTxSocket("192.168.1.11", 50000);
 
         std::cout << "UDP Unicast Tx Socket Open Success" << std::endl;
 #elif TEST_MODE == TEST_MODE_MULTICAST
         /* UDPマルチキャスト送信用ソケットオープン */
-        adapter.OpenUdpMultiTxSocket("224.0.0.32", "127.0.0.1", 50000, 1);
+        adapter.OpenUdpMultiTxSocket("224.0.0.32", "192.168.1.10", 50000, 1);
 
         std::cout << "UDP Multicast Tx Socket Open Success" << std::endl;
 #elif TEST_MODE == TEST_MODE_BROADCAST
